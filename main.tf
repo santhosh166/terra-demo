b0d9a3df37645805f04482a1837dc91931eb660f
Santhosh R S <65718917+santhosh166@users.noreply.github.com>
2022-03-18 13:18:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,20 @@ resource "azurerm_resource_group" "rg" {
   location = "southindia"
 }
 
+
+provider "azurerm" {
+
+  subscription_id = "742c1fc8-bf88-4a1f-8283-49b0e12d7eb2"
+  client_id       = "b811d451-d5b0-4e40-8f3b-055fac3f647f"
+  client_secret   = "cGR7Q~v4HzIRZLBYLI2jvbpANjzk5noUXDaR1"
+  tenant_id       = "70fda6a7-c2f8-467a-8264-8a1bf647a411"
+
+  features {}
+}
+
+provider "azuread" {
+  client_id       = "b811d451-d5b0-4e40-8f3b-055fac3f647f"
+  client_secret   = "cGR7Q~v4HzIRZLBYLI2jvbpANjzk5noUXDaR1"
+  tenant_id       = "70fda6a7-c2f8-467a-8264-8a1bf647a411"
+}
+
